dfa03244b02b506553a4fb7097769001e0009d3f
Sean O'Reilly <seansoreilly@gmail.com>
2024-12-08 18:10:45 +1100

    change caption bar change to "I can attend"

--- src/app/components/Scheduler.tsx
+++ src/app/components/Scheduler.tsx
@@ -306,7 +306,7 @@ const SimpleScheduler = () => {
                           <div className="w-11 h-6 bg-red-400 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
                         </div>
                         <span className="ms-3 text-sm font-medium text-gray-900">
-                          {attendees.includes(userName) ? "Could attend" : "Can't attend"}
+                          {attendees.includes(userName) ? "I could attend" : "I can't attend"}
                         </span>
                       </label>
                     </div>
